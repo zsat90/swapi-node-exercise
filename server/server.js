60856d9ea07e616d5809b1b295a1626a045302eb
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-05-17 10:01:57 -0400

    First commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cors = require("cors");
+
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Express is working! Yes sir");
+});
+
+// Get the people from SWAPI
+app.get("/people", (req, res) => {
+  axios
+    .get("https://swapi.dev/api/people")
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log("App listening on Port 3000");
+});
